d9a56108f384ae02e368114ca2900a5f90c53896
JustinT-02 <jtat233@gmail.com>
2023-08-28 15:07:37 -0400

    First commit

---
+++ areas.js
@@ -0,0 +1,19 @@
+const areaCircle = function(radius) {
+  return Math.PI * (radius ** 2);
+}
+
+const areaTriangle = function(base, height) {
+  return 0.5 * base * height;
+}
+
+const areaSquare = function(length) {
+  return length ** 2;
+}
+
+const areaRectangle = function(length, width) {
+  return length * width;
+}
+
+const areaParallelogram = function(base, verticalHeight) {
+  return base * verticalHeight;
+}
